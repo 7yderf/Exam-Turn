98e185cdc0bcd67286e8f0510101890e929c305f
7yderf <7yderf@gmail.com>
2022-08-01 20:22:08 -0500

    🎉 DEPLOYMENT

--- src/common/services/avaluo.js
+++ src/common/services/avaluo.js
@@ -1,8 +1,18 @@
 /* eslint-disable import/prefer-default-export */
 import config from "@config";
 
+const myHeaders = new Headers();
+myHeaders.append("Accept", "application/json");
+myHeaders.append("Cookie", "laravel_session=7xKzQEyGTuDpPdha7HdxL70iXkbJcvA3N5gk9rGO");
+
+const requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow"
+};
+
 export const infoAvaluo = async () => {
-  const request = await fetch(`${config.api}/api/leads/avaluos/1`);
+  const request = await fetch(`${config.api}/api/leads/avaluos/1`, requestOptions);
   const { data, error } = await request.json();
   if (error) throw new Error(error);
   return data;
